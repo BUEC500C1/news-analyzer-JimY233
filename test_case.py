20545ba4fc42085c56a4ba5bd1aa2b4ccd3a7174
JimY233 <54047662+JimY233@users.noreply.github.com>
2021-02-15 17:15:29 -0500

    Update test_case.py

--- test_case.py
+++ test_case.py
@@ -6,6 +6,21 @@ import securefileuploaderapi
 import NLPanalysisapi
 import newsingesterapi
 
+import tracemalloc
+
 def test_case():
   assert 1 == 1
+  
+
+tracemalloc.start()
+
+# ... run your application ...
+test_case()
+
+snapshot = tracemalloc.take_snapshot()
+top_stats = snapshot.statistics('lineno')
+
+print("[ Top 10 ]")
+for stat in top_stats[:10]:
+    print(stat)
 
