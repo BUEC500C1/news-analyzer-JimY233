8f619c8ceb351b61e15a8d4a96dea34bd6b2643e
JimY233 <54047662+JimY233@users.noreply.github.com>
2021-03-29 17:12:33 -0400

    Update test_case.py

--- test_case.py
+++ test_case.py
@@ -1,31 +1,32 @@
 # ============================================================
-# Defining your own testing here
+# For the result of AWS hosting, please check the screenshot on README.md
+# This test_case.py just test the functions used in the NLP API
 # ============================================================
 
+import pytest
 
-import tracemalloc
-import cProfile
+#search_nlp(search,text) #find the frequency of search in text
+from NLP.nlp.nlp_search import *
 
-import logging
+#NLP_analyze(text)
+from NLP.nlp.NLPAPI import *
 
-def test_case():
-  assert 1 == 1
-  
 
-tracemalloc.start()
+def test_NLP1():
+    text = 'Hello world!'
+    sentiment = NLP_analyze(text)
+    assert hasattr(sentiment,'score') == True
 
-# ... run your application ...
-test_case()
-
-snapshot = tracemalloc.take_snapshot()
-top_stats = snapshot.statistics('lineno')
-
-print("[ Top 10 ]")
-for stat in top_stats[:10]:
-    print(stat)
-
+def test_NLP2():
+    text = 'Hello world!'
+    sentiment = NLP_analyze(text)
+    assert hasattr(sentiment,'magnitude') == True
     
-cProfile.run("20+10")
-
-logging.warning('Watch out!')  # will print a message to the console
-logging.info('I told you so')  # will not print anything
+def test_NLP3():
+    text = "The COVID 19 pandemic has altered every aspect of American life, from health and work to education and exercise. Over the long term, warns the American Psychological Association, the negative mental health effects of the covid will be serious and long-lasting."
+    keyword = "covid"
+    freq = nlp_search(keyword,text)
+    assert freq == 2
+    
+if __name__ == '__main__':
+    pytest.main()
